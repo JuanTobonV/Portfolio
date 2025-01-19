41439f47a0a55340381e4739ef8ce5a72709116c
Juan Villa <jujotovil@gmail.com>
2025-01-19 14:14:35 -0500

    adding about me page

--- src/app/menuWrapper.js
+++ src/app/menuWrapper.js
@@ -1,5 +1,6 @@
 const menu = document.getElementById('menu');
 const menuInfo = document.getElementById('menuInfo');
+const navBar = document.getElementById("navBar");
 
 menu.addEventListener('click', () => {
     menuInfo.classList.toggle('hidden');
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       fontFamily: {
         'jet': ['JetBrains Mono', 'serif'],
       }
+
     },
   },
   plugins: [],
